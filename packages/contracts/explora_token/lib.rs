2c08e0a49543de68c58aa761242ce9e160de1b0a
beach-dev <joshua.jin.8321@gmail.com>
2023-07-28 11:30:42 -0400

    add explora token

--- packages/contracts/explora_token/lib.rs
+++ packages/contracts/explora_token/lib.rs
@@ -3,19 +3,16 @@
 #[openbrush::implementation(PSP34)]
 #[openbrush::contract]
 pub mod explora_token {
+    use ink::prelude::string::String;
     use openbrush::traits::Storage;
-    use secp256k1::{RecoveryId, Message, Signature, PublicKey, Secp256k1};    
+    use ink::env::hash::HashOutput;
+    use ink::env::hash::Keccak256;
 
     #[ink(storage)]
     #[derive(Default, Storage)]
     pub struct Contract {
         #[storage_field]
         psp34: psp34::Data,
-    }
-
-    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
-    pub struct Id {
-        value: u64,
     }
 
     impl Contract {
@@ -25,42 +22,25 @@ pub mod explora_token {
         }
 
         #[ink(message)]
-        pub fn mint(&mut self, id: Id, token_uri: String, signature: Vec<u8>) -> Result<(), PSP34Error> {
-            let caller = Self::env().caller();
+        pub fn mint(&mut self, id: Id, token_uri: String, signature: [u8; 65]) -> Result<(), PSP34Error> {
             let encoded_id = scale::Encode::encode(&id);
             let encoded_uri = token_uri.as_bytes();
-    
-            // Combine the ID and URI data to generate the message to be signed
             let mut message_to_sign = Vec::new();
             message_to_sign.extend_from_slice(&encoded_id);
             message_to_sign.extend_from_slice(&encoded_uri);
-    
-            // Verify the signature using the provided public key
-            let public_key = self.psp34.public_keys.get(&caller).ok_or(PSP34Error::InvalidCaller)?;
-            if !verify_signature(&signature, &message_to_sign, &public_key) {
-                return Err(PSP34Error::InvalidSignature);
+			
+            if !verify_signature(&signature, &message_to_sign) {
+                return Err(PSP34Error::Custom(String::from("invalid signature")))
             }
-    
-            // Mint the token if the signature is valid
-            psp34::Internal::_mint_to(self, caller, id)?;
-    
-            Ok(())
+            psp34::Internal::_mint_to(self, Self::env().caller(), id);
+			Ok(())
         }
     }
-    
-    fn verify_signature(signature: &[u8], message: &[u8], public_key: &[u8]) -> bool {
-        let secp = Secp256k1::verification_only();
-        let message = Message::parse_slice(message).expect("Invalid message");
-        let signature = Signature::parse_slice(signature).expect("Invalid signature");
-        let public_key = PublicKey::parse_slice(public_key).expect("Invalid public key");
-
-        let recovery_id = RecoveryId::parse(signature[64]).expect("Invalid recovery ID");
-        let message_hash = secp256k1::Message::from_slice(&message);
-        if let Ok(message_hash) = message_hash {
-            if let Ok(recovered_public_key) = secp.recover(&message_hash, &signature, &recovery_id) {
-                return recovered_public_key.serialize_uncompressed() == public_key.serialize_uncompressed();
-            }
-        }
-        false
+    fn verify_signature(signature: &[u8; 65], message: &[u8]) -> bool {
+        let mut message_hash = <Keccak256 as HashOutput>::Type::default();
+        ink::env::hash_encoded::<Keccak256, _>(&message, &mut message_hash);
+        let mut output = [0; 33];
+        ink::env::ecdsa_recover(signature, &message_hash, &mut output);
+        true
     }
 }
